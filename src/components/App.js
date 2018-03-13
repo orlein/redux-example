ae52112022971b77cd4871989c4b2722a16421d3
PARK,Changjun <pcj9024@gmail.com>
2018-03-13 23:29:41 +0900

    하다말았음 피곤함

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class App extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        YAPYAPYAP
+      </div>
+    )
+  }
+}
+
+export default App
